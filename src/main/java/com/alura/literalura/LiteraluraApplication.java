e60f37a8cd949761f7f39adbe836ee7fd4757cd6
Luis Herrera <luis1818.lh@gmail.com>
2024-11-20 21:12:19 -0500

    configuración de conexión con la base de datos check

--- src/main/java/com/alura/literalura/LiteraluraApplication.java
+++ src/main/java/com/alura/literalura/LiteraluraApplication.java
@@ -1,13 +1,28 @@
 package com.alura.literalura;
 
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LiteraluraApplication {
+public class LiteraluraApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+		dotenv.entries().forEach(entry ->
+				System.setProperty(entry.getKey(), entry.getValue())
+		);
+
 		SpringApplication.run(LiteraluraApplication.class, args);
+
+
+
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Hello World");
 	}
 
 }
